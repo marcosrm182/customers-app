afea459beda6888f43c246a59004ea74353ab2df
Marcos Rodriguez Morales <contacto@gorilast.com>
2019-02-27 23:01:03 +0100

    Añadido withRouter y capturado evento boton nuevo cliente para redireccionar

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import AppFrame from '../components/AppFrame';
 import CustomersList from '../components/CustomersList';
 import CustomersActions from '../components/CustomersActions';
@@ -24,6 +25,10 @@ const customers = [
 
 class CustomersContainer extends Component {
     
+    handleAddNew = () => {
+        this.props.history.push('/customers/new');
+    }
+
     renderBody = customers => (
         <div>
             <CustomersList
@@ -51,4 +56,4 @@ CustomersContainer.propTypes = {
 
 };
 
-export default CustomersContainer;
+export default withRouter(CustomersContainer);
