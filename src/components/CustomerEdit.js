ee0b8eb3b3747e1af6401a967d706866f7f37d87
Marcos Rodriguez Morales <contacto@gorilast.com>
2019-03-21 10:00:34 +0100

    Validaciones múltiples en fields y mejoras a MyField

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -3,23 +3,48 @@ import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
+const isRequired = value => (
+    !value && "Este campo es requerido!!"
+);
+
+const isNumber = value => (
+    isNaN(Number(value)) && "El campo debe ser un número"
+);
+
+const MyField = ({input, meta, type, label, name}) => (
+    <div>
+        <label htmlFor={name}>{label}</label>
+        <input {...input} type={!type ? "text" : type} />
+        {
+            meta.touched && meta.error && <span>{meta.error}</span>
+        }
+    </div>
+);
+
 const CustomerEdit = ({ name, dni, age}) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
             <form action="">
-                <div>
-                    <label htmlFor="name">Nombre</label>
-                    <Field name="name" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="dni">Dni</label>
-                    <Field name="dni" component="input" type="text"></Field>
-                </div>
-                <div>
-                    <label htmlFor="age">Edad</label>
-                    <Field name="age" component="input" type="number"></Field>
-                </div>
+                <Field
+                    name="name"
+                    component={MyField}
+                    validate={isRequired}
+                    label="Nombre">
+                </Field>
+                <Field
+                    name="dni"
+                    component={MyField}
+                    validate={[isRequired, isNumber]}
+                    label="Dni">
+                </Field>
+                <Field
+                    name="age"
+                    component={MyField}
+                    type="number"
+                    validate={isNumber}
+                    label="Edad">
+                </Field>
             </form>
 
         </div>
