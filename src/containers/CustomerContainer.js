0c70c2b6eb452d32be430606bcb3499899909ab4
Marcos Rodriguez Morales <contacto@gorilast.com>
2019-03-26 17:18:12 +0100

    Validación de servidor con SubmissionError

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -8,6 +8,7 @@ import CustomerEdit from './../components/CustomerEdit';
 import CustomerData from './../components/CustomerData';
 import { fetchCustomers } from './../actions/fetchCustomers';
 import { updateCustomer } from '../actions/updateCustomer';
+import { SubmissionError } from 'redux-form';
 
 class CustomerContainer extends Component {
     //<p>Datos del Cliente {this.props.customer.name}</p>
@@ -21,7 +22,11 @@ class CustomerContainer extends Component {
     handleSubmit = values => {
         console.log(JSON.stringify(values));
         const { id } = values;
-        return this.props.updateCustomer(id, values);
+        return this.props.updateCustomer(id, values).then( r => {
+            if (r.error) {
+                throw new SubmissionError(r.payload);
+            }
+        });
     }
 
     handlOnBack = () => {
